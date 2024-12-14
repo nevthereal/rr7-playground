524f793282c15d5c0e9557111d4e71e4c29e04d1
Neville Brem <bremneville@gmail.com>
2024-12-14 19:35:09 +0100

    first few changes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,18 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "React Router Playground" },
+    { name: "description", content: "idk" },
   ];
 }
 
+export async function clientLoader() {}
+
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  );
 }
